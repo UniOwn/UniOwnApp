8dae8a0e63691dae92aa3f96e530b980cc079723
Ilya Apaniuk <i.apaniuk@reply.com>
2023-10-23 21:14:58 +0200

    feat (cookieParser): added cookie-parser

--- backend/db_service/src/utils/index.ts
+++ backend/db_service/src/utils/index.ts
@@ -1,3 +1,4 @@
 export { default as transaction } from "./mongo-transaction/mongo-transaction";
+export { default as ParseCookies } from "./parse-cookies/parse-cookies.decorator";
+export { default as ParseObjectIdPipe } from "./parse-object-id/parse-object-id.pipe";
 export { default as HttpExceptionFilter } from "./http-exception/http-exception.filter";
-export { default as ParseObjectIdPipe } from "./parse-object-id-pipe/parse-object-id-pipe";
