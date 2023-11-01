65c2d920fe7081215428779952bbf5cca4a7c587
Ilya Apaniuk <i.apaniuk@reply.com>
2023-11-01 18:02:49 +0100

    feat (navbar): added navbar

--- frontend/app/src/app/page.tsx
+++ frontend/app/src/app/page.tsx
@@ -1,34 +1,28 @@
-import { getServerSession } from "next-auth";
+// import { getServerSession } from "next-auth";
 
-import { AssetCard } from "@/components/Cards";
-import ConnectWalletButton from "@/components/Buttons/ConnectToWallet/ConnectToWallet";
+// import { AssetCard } from "@/components/Cards";
 
-import { authOptions } from "./api/auth/[...nextauth]/route";
+// import { authOptions } from "./api/auth/[...nextauth]/route";
 
-async function getData() {
-    const session: any = await getServerSession(authOptions);
+// async function getData() {
+//     const session: any = await getServerSession(authOptions);
 
-    const res = await fetch("http://localhost:5005/users/653fe7869fb4efe4ffe2e1ff", {
-        method: "GET",
-        headers: { authorization: `Bearer ${session?.tokens.access_token}`, "Content-Type": "application/json" }
-    });
-    // The return value is *not* serialized
-    // You can return Date, Map, Set, etc.
+//     const res = await fetch("http://localhost:5005/users/653fe7869fb4efe4ffe2e1ff", {
+//         method: "GET",
+//         headers: { authorization: `Bearer ${session?.tokens.access_token}`, "Content-Type": "application/json" }
+//     });
+//     // The return value is *not* serialized
+//     // You can return Date, Map, Set, etc.
 
-    if (!res.ok) {
-        // This will activate the closest `error.js` Error Boundary
-    }
+//     if (!res.ok) {
+//         // This will activate the closest `error.js` Error Boundary
+//     }
 
-    return res.json();
-}
+//     return res.json();
+// }
 
 export default async function Home() {
-    const data = await getData();
+    // const data = await getData();
 
-    return (
-        <main>
-            <ConnectWalletButton />
-            {data?.username && <AssetCard username={data?.username} />}
-        </main>
-    );
+    return <main></main>;
 }
--- frontend/app/src/components/AppHeader/AppHeader.tsx
+++ frontend/app/src/components/AppHeader/AppHeader.tsx
@@ -1,11 +1,44 @@
-import { SearchInput } from "@/controls";
+"use client";
+
+import Image from "next/image";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
+
+import logo from "../../../public/images/logo.png";
 
 import "./AppHeader.style.scss";
 
 const AppHeader = () => {
     return (
         <div className="AppHeader">
-            <SearchInput />
+            <Navbar shouldHideOnScroll maxWidth="full">
+                <NavbarBrand>
+                    <Image src={logo} alt="" sizes="100vw" className="AppHeader-Logo" />
+                </NavbarBrand>
+                <NavbarContent className="hidden sm:flex gap-4" justify="center">
+                    <NavbarItem>
+                        <Link color="foreground" href="#">
+                            Games
+                        </Link>
+                    </NavbarItem>
+                    <NavbarItem>
+                        <Link href="#" aria-current="page">
+                            Assets
+                        </Link>
+                    </NavbarItem>
+                </NavbarContent>
+                <NavbarContent justify="end">
+                    <NavbarItem className="hidden lg:flex">
+                        <Button as={Link} color="primary" href="#" variant="flat">
+                            Login
+                        </Button>
+                    </NavbarItem>
+                    <NavbarItem>
+                        <Button as={Link} color="primary" href="#" variant="flat">
+                            Sign Up
+                        </Button>
+                    </NavbarItem>
+                </NavbarContent>
+            </Navbar>
         </div>
     );
 };
